50f1938e49a12c1f4b4223839f2ea482493fa6d9
Tony Melek <tonymelek.au@gmail.com>
2020-10-24 12:25:51 +1100

    re-implement with express handlebars

--- index.js
+++ index.js
@@ -3,60 +3,45 @@ const fs = require('fs')
 const path = require('path')
 const mdWriter = require('./routes/functions/mdwriter')
 const app = express()
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5500
+const expbs = require('express-handlebars')
 
-//Read download.html
-let download;
-fs.readFile(path.join(__dirname, 'public', 'download.html'), 'utf8', (err, data) => {
-    if (err) {
-        console.log(err);
-    } else {
-        download = data
-    }
-})
+//Identify the use of Handlebars
+app.engine('handlebars', expbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars')
+
 
 //Handle the Post request and response
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 //Adding Static Route
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'views')))
 
-//Adding route for the download page
-app.get('/download', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'download.html'))
-})
-//Adding route for the temp page
-app.get('/temp', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'temp.html'))
-})
-
-//Adding POST Endpoint
-app.post('/api/generate', (req, res) => {
-    const mdText = mdWriter(req.body)
-
-    fs.writeFile(path.join(__dirname, 'public/output', `README.md`), mdText, err => {
-        if (err) {
-            return console.log(err)
-        }
-        else {
-            let downloadHTML = download
-            downloadHTML = downloadHTML.replace('{{mdFileText}}', mdText)
-            downloadHTML = downloadHTML.replace('{{mdFilePath}}', `./output/README.md`)
-            fs.writeFile(path.join(__dirname, 'public', 'temp.html'), downloadHTML, err => {
-                if (err) {
-                    return console.log(err);
-                } else {
-                    console.log('file generted successfully')
-                    res.redirect('/temp')
-                }
-            })
-
-        }
+//Home Page
+app.get('/', (req, res) => {
+    res.render('index', {
+        'title': 'Home'
     })
 })
 
 
+//Adding POST Endpoint
+app.post('/api/generate', (req, res) => {
+    const mdText = mdWriter(req.body)
+    res.render('download', {
+        'title': 'Download README',
+        'mdFileText': mdText,
+        'mdFilePath': '../output/README.md'
+    })
+    fs.writeFile(path.join(__dirname, 'views/output', `README.md`), mdText, err => {
+        if (err) {
+            return console.log(err)
+        }
+        console.log('README.md file generted successfully')
+    })
+
+})
 
 //Initialising the server
 app.listen(PORT, () => console.log(`Server is now up and running on port ${PORT}`))
--- views/output/README.md
+++ views/output/README.md
@@ -1,5 +1,5 @@
 ![License: 'GPL v3'](https://img.shields.io/badge/License-GPLv3-blue.svg)
-# dasdsadsad
+# 
 * [Description](#description)
 * [Installation](#installation)
 * [Usage](#usage)
@@ -8,18 +8,18 @@
 * [Tests](#tests)
 * [Questions](#questions)
 ## Description
-dasdada
+
 ## Installation
-dasdad
+
 ## Usage
-dasdadfrerfgdfg
+
 ## Contributing
-ewrwrrwf
+
 ## Tests
-ewrwer
+
 ## Questions
 You are welcome to provide any feedback and/or ask questions.
-Please, send any question to my e-mail [erwer@GDGDFG](mailto:erwer@GDGDFG) and/or visit my profile on [Github](https://github.com/DFSFDS)
+Please, send any question to my e-mail [](mailto:) and/or visit my profile on [Github](https://github.com/)
 
 ## License
 The project is protected under GPL V3,you may need to read through license conditions
